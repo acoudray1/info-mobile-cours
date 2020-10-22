694fc75eb0a417d357ea7377a1e12793ca4a372f
acoudray1 <axel.hpm.coudray@gmail.com>
2020-10-22 14:34:44 +0200

    added connection informations

--- 03-cordova/www/js/index.js
+++ 03-cordova/www/js/index.js
@@ -29,4 +29,21 @@ function onDeviceReady() {
     document.getElementById('cordovaversion').innerHTML = "CORDOVA : " + cordova.version;
     document.getElementById('devicemodel').innerHTML = "MODEL : " + device.model;
     document.getElementById('uuid').innerHTML = "UUID : " + device.uuid;
+    document.getElementById('connectiontype').innerHTML = checkConnection();
+}
+
+function checkConnection() {
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+
+    return 'Connection type: ' + states[networkState];
 }
